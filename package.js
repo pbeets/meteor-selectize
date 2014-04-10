ce2ccbc28bf879bcc5e6409472733673be184c06
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-04-10 05:21:26 -0700

    update

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "Easy responsive tabs"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files(['Easy-Responsive-Tabs-to-Accordion/js/easyResponsiveTabs.js'], 'client');
+});
