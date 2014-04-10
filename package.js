2bf47922a6b964ca58ad58cfa0e92c9cf9ea8be3
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-04-10 05:34:26 -0700

    update

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  summary: "Easy responsive tabs"
+  summary: "Selectize is the hybrid of a textbox and <select> box. It's jQuery based and it has autocomplete and native-feeling keyboard navigation; useful for tagging, contact lists, etc."
 });
 
 Package.on_use(function (api, where) {
-  api.add_files(['Easy-Responsive-Tabs-to-Accordion/js/easyResponsiveTabs.js'], 'client');
+  api.add_files(['selectize.js/dist/js/standalone/selectize.js'], 'client');
 });
